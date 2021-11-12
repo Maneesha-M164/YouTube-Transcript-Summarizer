aa1111d8f3950453430676aa0b8ef0ecf6357574
19IT114 <19it114@charusat.edu.in>
2021-11-12 22:03:56 +0530

    updated functionallity #1.2

--- youtube-transcript-summarizer-api/download.py
+++ youtube-transcript-summarizer-api/download.py
@@ -1,5 +1,5 @@
 def makeTextFile(name, content):
-    file = open(f"../youtube-transcript-summarizer-frontend/src/transcripts/{name}.txt","w")
+    file = open(f"../youtube-transcript-summarizer-frontend/src/transcripts/{name}.txt","w",encoding="utf-8")
     
     file.write(f"{name} Transcript:\n")
     file.write(content)
